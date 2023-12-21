ca44fa9ee842bc3ab60b231492013edf8289882b
Marlon-03 <marlonmovilla03@gmail.com>
2023-12-21 14:31:06 +0800

    retry

--- resources/js/dashboard/CategoriesCreate.vue
+++ resources/js/dashboard/CategoriesCreate.vue
@@ -14,6 +14,7 @@
 
 <script>
 export default {
+  emits: ['showNavbar'],
   data() {
     return {
       category: {
--- resources/js/dashboard/CategoriesList.vue
+++ resources/js/dashboard/CategoriesList.vue
@@ -23,6 +23,7 @@
 
 <script>
 export default{
+  emits: ['showNavbar'],
     data(){
         return{
             categories: {
