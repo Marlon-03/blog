fc9e34b4b3d170526e567410d68c593f88cb14b3
Marlon-03 <marlonmovilla03@gmail.com>
2024-01-04 15:36:20 +0800

    Update SingleBlog.vue

--- resources/js/pages/SingleBlog.vue
+++ resources/js/pages/SingleBlog.vue
@@ -8,6 +8,21 @@
         <img :src="`${post.imagePath}`" alt="">
         {{ post.body }}
     </section>
+
+    <section class="related">
+        <p>Related Posts</p>
+        <div class="related-posts">
+            <router link v-for="relatedPost in relatedPosts" :key="relatedPost.id"
+            :to="{name: 'SingleBlog', params: {slug: relatedPost.slug},}">
+            <div class="related-post">
+                <img :src="`${relatedPost.imagePath}`" alt="" loading="lazy">
+                <h4>{{ relatedPost.title }}</h4>
+            </div>
+
+            </router>
+        </div>
+
+    </section>
 </template>
 
 <script>
