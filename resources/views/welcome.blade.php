e8683609849ca3b49c2d29fcb0749db49af66214
Marlon-03 <marlonmovilla03@gmail.com>
2024-01-04 10:29:56 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="A laravel and vue blog website">
         <title>Laravue-Blog</title>
         @vite('resources/js/app.js')
     </head>
