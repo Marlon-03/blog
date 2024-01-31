474ae4361672cc5e20808203e6b4a463a20b9410
Marlon-03 <marlonmovilla03@gmail.com>
2024-01-31 14:26:56 +0800

    Update SingleBlog.vue

--- resources/js/pages/SingleBlog.vue
+++ resources/js/pages/SingleBlog.vue
@@ -1,28 +1,41 @@
 <template>
-    <section class="single-blog-post">
-        <img :src="post.imagePath + '?' + new Date().getTime()" alt="Post image">
-        <h1>{{ post.title }}</h1>
-        <p>
-            {{ post.created_at }}
-            <span>Written by{{ post.user }}</span>
-        </p>
-        <img :src="post.imagePath + '?' + new Date().getTime()" alt="Post image">
-        {{ post.body }}
-    </section>
-
-    <section class="related">
-        <p>Related Posts</p>
-        <div class="related-posts">
-            <router-link v-for="relatedPost in relatedPosts" :key="relatedPost.id"
-            :to="{name: 'SingleBlog', params: {slug: relatedPost.slug}}">
-            <div class="related-post">
-                <img :src="`${relatedPost.imagePath}`" alt="" loading="lazy">
-                <h4>{{ relatedPost.title }}</h4>
+    <section class="single-blog-post container mx-auto">
+        <img :src="post.imagePath + '?' + new Date().getTime()" alt="Post image" class="w-full  h-[450px] object-cover">
+        <div class="container px-20 py-10">
+            <h1 class="text-black font-bold font-istok-web text-4xl">{{ post.title }}</h1>
+            <div class="flex justify-between mb-2">
+                <span>Written by {{ post.user }}</span>
+                <span>{{ formatDate(post.created_at)}}</span>
             </div>
-            </router-link>
+            {{ post.body }}
         </div>
-
     </section>
+
+<section class="related py-20 max-w-custom mx-auto">
+    <div class="line-1 h-px bg-[#D9D9D9] my-1.5"></div>
+    <h1 class="font-kadwa text-4xl font-bold text-center mb-10 pt-5">Related Posts</h1>
+    <div class="related-posts flex justify-around">
+<router-link class="card-blog-content flex flex-col items-center bg-white rounded-lg md:w-[350px] my-4" 
+             v-for="relatedPost in relatedPosts.slice(0, 3)" 
+             :key="relatedPost.id"
+             :to="{name: 'SingleBlog', params: {slug: relatedPost.slug}}">
+    <div class="w-full">
+        <img :src="`${relatedPost.imagePath}`" alt="Post images" loading="lazy" class="w-full h-48 object-cover rounded-t">
+    </div>
+    <div class="w-full px-5 py-5">
+        <h4 class="font-bold">{{ relatedPost.title }}</h4>
+        <div class="line-clamp-2 overflow-hidden overflow-ellipsis">
+            {{ relatedPost.body }}
+        </div>
+        <div class="line-1 h-px bg-[#D9D9D9] my-1.5"></div>
+        <div class="flex justify-between">
+            <span>Written by {{ relatedPost.user }}</span>
+            {{ formatDate(relatedPost.created_at)}}
+        </div>
+    </div>
+</router-link>
+    </div>
+</section>
 </template>
 
 <script>
@@ -39,6 +52,7 @@ export default{
         axios.get('/api/posts/' + this.slug)
         .then((response) => {
             this.post = response.data.data;
+            window.scrollTo(0, 0); // Scroll to top after post data has been loaded
         }).catch((error) => {
             console.log(error);
         });
@@ -46,9 +60,16 @@ export default{
         axios.get('/api/related-posts/' + this.slug)
     .then((response) => {
         this.relatedPosts = response.data.data;
+        window.scrollTo(0, 0); // Scroll to top after related posts data has been loaded
     }).catch((error) => {
         console.log(error);
     });
+    },
+    methods:{
+        formatDate(dateString) {
+            const date = new Date(dateString);
+            return date.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' });
+        },
     }
 }
 </script>
