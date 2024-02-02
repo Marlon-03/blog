9352afa706d02d50083702db7187d841189dbbd2
Marlon-03 <marlonmovilla03@gmail.com>
2024-02-02 08:09:24 +0800

    Update Contact.vue

--- resources/js/pages/Contact.vue
+++ resources/js/pages/Contact.vue
@@ -8,20 +8,20 @@
     </div>
 
     <div class="bg-[#F2F1E9] w-full py-20 px-20">
-        <iv class="tab-content bg-white rounded-md px-10 py-10">
+        <div class="tab-content bg-white rounded-md px-10 py-10">
             <div class="flex gap-5">
                 <div class="w-[50%] flex flex-col">
-                    <input type="text" placeholder="Name" class="border border-1 rounded-3xl border-gray-300 p-2 pl-3 focus:outline-none focus:border-[#58AB91]"/><br>
-                    <input type="email" placeholder="Email" class="border border-1 rounded-3xl border-gray-300 p-2 pl-3 focus:outline-none focus:border-[#58AB91]"/><br>
-                    <input type="text" placeholder="Subject" class="border border-1 rounded-3xl border-gray-300 p-2 pl-3 focus:outline-none focus:border-[#58AB91]"/><br>
-                    <input type="description" placeholder="Message" class="border border-1 rounded-3xl border-gray-300 p-2 pl-3 focus:outline-none focus:border-[#58AB91]"/><br>
+                    <input type="text" placeholder="Name" class="border border-1 rounded-3xl border-gray-300 p-2 pl-3 focus:outline-none focus:border-[#58AB91]"/>
+                    <input type="email" placeholder="Email" class="border border-1 rounded-3xl border-gray-300 p-2 pl-3 focus:outline-none focus:border-[#58AB91]"/>
+                    <input type="text" placeholder="Subject" class="border border-1 rounded-3xl border-gray-300 p-2 pl-3 focus:outline-none focus:border-[#58AB91]"/>
+                    <input type="description" placeholder="Message" class="border border-1 rounded-3xl border-gray-300 p-2 pl-3 focus:outline-none focus:border-[#58AB91]"/>
                 </div>
 
                 <div class="w-[50%]">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, beatae voluptatibus. Quo quod provident, nam eius expedita dignissimos ad sunt aliquid pariatur velit accusamus saepe suscipit consequatur voluptatum cupiditate quos ipsa necessitatibus atque illum obcaecati? Corporis minima odit fugiat impedit.</p>
                 </div>
             </div>
-        </iv>
+        </div>
     </div>
 
 
