55a86f5dbcccfa6ebabdab83f8e4ad0b4ba22289
Marlon-03 <marlonmovilla03@gmail.com>
2024-02-05 09:44:08 +0800

    admindashboard style

--- resources/js/pages/AdminDashboard.vue
+++ resources/js/pages/AdminDashboard.vue
@@ -1,43 +1,64 @@
 <template>
     <div>
-    <div class="flex justify-between px-20 py-5">
+    <div class="flex justify-between items-center px-20 py-5">
         <h1 class="font-kadwa font-medium text-xl">Hi, Admin  {{ name }}</h1>
-        <button @click="logout" class="font-kadwa font-medium text-xl">Log Out</button>
+        <button @click="logout" class="font-kadwa font-medium text-xl bg-[#D9D9D9] px-3 py-2 rounded-md">Log Out</button>
     </div>
     <div class="line-2 h-px bg-black my-1.5"></div>
     </div>
 
-    <div> 
-        <div>
-            <router-link :to="{name: 'CreatePost'}">Create Post</router-link>
-        </div>
+    <div class="flex flex-wrap">
+        <router-link class="bg-[#58AB91] rounded-md" :to="{name: 'CategoriesCreate'}">
+            <div class="justify-center items-center flex-col flex px-4 py-8">
+                <img :src="createIcon" class="w-10 h-10">
+                <p class="font-istok-web font-bold text-white text-2xl">Create category</p>
+            </div>
+        </router-link>
 
-        <div>
-            <router-link :to="{name: 'CategoriesCreate'}">Create Categories</router-link>
-        </div>
+        <router-link class="bg-[#58AB91] rounded-md" :to="{name: 'AdminCategoriesList'}">
+            <div class="justify-center items-center flex-col flex px-4 py-8">
+                <img :src="createIcon" class="w-10 h-10">
+                <p class="font-istok-web font-bold text-white text-2xl">Categories List</p>
+            </div>
+        </router-link>
 
-        <div>
-            <router-link :to="{name: 'AdminCategoriesList'}">Category List</router-link>
-        </div>
+        <router-link class="bg-[#58AB91] rounded-md" :to="{name: 'AdminApprovalPosts'}">
+            <div class="justify-center items-center flex-col flex px-4 py-8">
+                <img :src="createIcon" class="w-10 h-10">
+                <p class="font-istok-web font-bold text-white text-2xl">Approval posts</p>
+            </div>
+        </router-link>
 
-        <div>
-            <router-link :to="{name: 'AdminApprovalPosts'}">Approval Posts</router-link>
-        </div>
+        <router-link class="bg-[#58AB91] rounded-md" :to="{name: 'CreatePost'}">
+            <div class="justify-center items-center flex-col flex px-4 py-8">
+                <img :src="createIcon" class="w-10 h-10">
+                <p class="font-istok-web font-bold text-white text-2xl">Write a post</p>
+            </div>
+        </router-link>
 
-        <div>
-            <router-link :to="{name: 'PostsList'}">Post Lists</router-link>
-        </div>
-        
+        <router-link class="bg-[#58AB91] rounded-md" :to="{name: 'PostsList'}">Post Lists
+            <div class="justify-center items-center flex-col flex px-4 py-8">
+                <img :src="createIcon" class="w-10 h-10">
+                <p class="font-istok-web font-bold text-white text-2xl">Your posts</p>
+            </div>
+        </router-link>
+
     </div>
 </template>
 
 
 <script>
+import createIcon from '../../../public/img/create.svg';
+import notesIcon from '../../../public/img/notes.svg';
+import writeIcon from '../../../public/img/write.svg';
 export default{
     emits: ['showNavbar'], 
     data(){
         return{
             name: '',
+            createIcon,
+            notesIcon,
+            writeIcon
         };
     },
     mounted() {
