e946825fe73071a527af69f38a4c598a0fdfc57e
Marlon-03 <marlonmovilla03@gmail.com>
2024-02-08 16:38:50 +0800

    Fixing the responsiveness

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,61 +1,75 @@
 <template>
-    <div v-if="showNavbar" >
-        <div className="flex justify-around items-center py-4 bg-[#58AB91]">
-            <div className="flex items-center">
-                <img src="../../img/vite.png" alt="Logo" class="h-10 w-10" />
-            </div>
-            <div className="space-x-10 text-white font-semibold text-xl">
-                <router-link :to="{name: 'Home'}">Home</router-link>
-                <router-link :to="{name: 'Blog'}">Blog</router-link>
-                <router-link :to="{name: 'About'}">About</router-link>
-                <router-link :to="{name: 'Contact'}">Contact</router-link>
-            </div>
-            <div className="space-x-10 text-white font-semibold text-xl">
-                <router-link :to="{name: 'Login'}" v-if="!isLoggedIn" @click="handleLoginClick">Log In</router-link>
-                <router-link :to="{name: 'Register'}" v-if="!isLoggedIn" @click="handleRegisterClick">Register</router-link>
-                <router-link :to="{name: 'Dashboard'}" v-if="isLoggedIn">Dashboard</router-link>
-            </div>
+    <div v-if="showNavbar">
+      <div class="flex justify-around items-center py-4 bg-[#58AB91] lg:flex-row flex-col">
+        <div class="flex items-center">
+          <img src="../../img/vite.png" alt="Logo" class="h-10 w-10" />
         </div>
+        <button @click="isOpen = !isOpen" class="lg:hidden">
+            <img :src="hamburgerIcon" class="absolute w-7 h-7 ml-1">
+        </button>
+        <div v-if="isOpen || isLargeScreen" class="lg:flex hidden space-x-10 text-white font-semibold text-xl">
+          <router-link :to="{name: 'Home'}">Home</router-link>
+          <router-link :to="{name: 'Blog'}">Blog</router-link>
+          <router-link :to="{name: 'About'}">About</router-link>
+          <router-link :to="{name: 'Contact'}">Contact</router-link>
+        </div>
+        <div v-if="isOpen || isLargeScreen" class="lg:flex hidden space-x-10 text-white font-semibold text-xl">
+          <router-link :to="{name: 'Login'}" v-if="!isLoggedIn" @click="handleLoginClick">Log In</router-link>
+          <router-link :to="{name: 'Register'}" v-if="!isLoggedIn" @click="handleRegisterClick">Register</router-link>
+          <router-link :to="{name: 'Dashboard'}" v-if="isLoggedIn">Dashboard</router-link>
+        </div>
+      </div>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  import hamburgerIcon from '../../../public/img/hamburger.svg';
+  export default {
     data() {
-        return {
-            isLoggedIn: false,
-            isOpen: false,
-        };
+      return {
+        hamburgerIcon,
+        isLoggedIn: false,
+        isOpen: false,
+        isLargeScreen: window.innerWidth > 768,
+
+      };
     },
     computed: {
-        showNavbar() {
-            return this.$route.name !== 'Login' && this.$route.name !== 'Register';
-        }
+      showNavbar() {
+        return this.$route.name !== 'Login' && this.$route.name !== 'Register';
+      }
     },
     created() {
-        this.checkAuthentication();
+      this.checkAuthentication();
+      window.addEventListener('resize', this.updateWindowSize);
+    },
+    beforeDestroy() {
+      window.removeEventListener('resize', this.updateWindowSize);
     },
     watch: {
-        '$route': function() {
-            this.checkAuthentication();
-        },
+      '$route': function() {
+        this.checkAuthentication();
+      },
     },
     methods: {
-        handleLoginClick() {
-            this.$emit('handleLoginClick');
-            this.isOpen = false;
-        },
-        handleRegisterClick() {
-            this.$emit('handleRegisterClick');
-            this.isOpen = false;
-        },
-        checkAuthentication() {
-            this.isLoggedIn = !!localStorage.getItem('authenticated');
-        },
+      handleLoginClick() {
+        this.$emit('handleLoginClick');
+        this.isOpen = false;
+      },
+      handleRegisterClick() {
+        this.$emit('handleRegisterClick');
+        this.isOpen = false;
+      },
+      checkAuthentication() {
+        this.isLoggedIn = !!localStorage.getItem('authenticated');
+      },
+      updateWindowSize() {
+        this.isLargeScreen = window.innerWidth > 768;
+      }
     },
-};
-</script>
-
-<style scoped>
-
-</style>
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -2,13 +2,13 @@
     <div class="relative background flex items-center justify-center h-3/4">
         <div class=" flex flex-col gap-y-36">
             <div class="hero-content flex flex-col gap-y-5 items-center text-white">
-                <h1 class="font-kadwa text-8xl">All About Me</h1>
+                <h1 class="font-kadwa xl:text-8xl text-5xl md:text-6xl lg:text-6xl">All About Me</h1>
             </div>
         </div>
     </div>
 
     <div class="bg-[#F2F1E9] w-full py-20 px-10">
-        <div class="tabs w-full flex justify-center gap-5 pb-8">
+        <div class="tabs w-full flex flex-col justify-center text-center md:flex-row gap-5 pb-8">
             <div class="tab font-kadwa font-semibold text-3xl" 
                  v-for="(tab, index) in tabs" 
                  :key="index" 
@@ -18,13 +18,13 @@
             </div>
         </div>
 
-        <div class="tab-content bg-white rounded-md px-5 py-5">
-            <div v-if="activeTab === 'My Story'" class="flex">
-                <div class="w-[40%]">
+        <div class="tab-content bg-white  rounded-md px-5 py-5">
+            <div v-if="activeTab === 'My Story'" class="flex justify-between gap-5 flex-col md:flex-row ">
+                <div class="md:w-[35%] w-[100%]">
                     <img src="../../../public/img/king.jpg"
                     class="w-96 h-80 object-cover"/>
                 </div>
-                <div class="w-[60%]">
+                <div class="md:w-[55%] w-[100%]">
                     <p class="text-lg font-istok-web indent">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus vel magni enim eligendi, 
                         dolorem id fugit asperiores, distinctio praesentium voluptate ad modi unde et fugiat saepe 
                         perspiciatis nemo. Asperiores ex ut voluptate. Illum mollitia nobis cum suscipit id fuga autem.<br>
@@ -35,12 +35,12 @@
                     </p>
                 </div>
             </div>
-            <div v-if="activeTab === 'Experience'" class="flex">
-                <div class="w-[40%]">
+            <div v-if="activeTab === 'Experience'" class="flex justify-between gap-5 flex-col md:flex-row ">
+                <div class="md:w-[35%] w-[100%]">
                     <img src="../../../public/img/rook.jpeg"
                     class="w-96 h-80 object-cover"/>
                 </div>
-                <div class="w-[60%]">
+                <div class="md:w-[55%] w-[100%]">
                     <p class="text-lg font-istok-web indent">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus vel magni enim eligendi, 
                         dolorem id fugit asperiores, distinctio praesentium voluptate ad modi unde et fugiat saepe 
                         perspiciatis nemo. Asperiores ex ut voluptate. Illum mollitia nobis cum suscipit id fuga autem.<br>
@@ -51,12 +51,12 @@
                     </p>
                 </div>
             </div>
-            <div v-if="activeTab === 'Education'" class="flex">
-                <div class="w-[40%]">
+            <div v-if="activeTab === 'Education'" class="flex justify-between gap-5 flex-col md:flex-row ">
+                <div class="md:w-[35%] w-[100%]">
                     <img src="../../../public/img/knight.jpeg"
                     class="w-96 h-80 object-cover"/>
                 </div>
-                <div class="w-[60%]">
+                <div class="md:w-[55%] w-[100%]">
                     <p class="text-lg font-istok-web indent">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus vel magni enim eligendi, 
                         dolorem id fugit asperiores, distinctio praesentium voluptate ad modi unde et fugiat saepe 
                         perspiciatis nemo. Asperiores ex ut voluptate. Illum mollitia nobis cum suscipit id fuga autem.<br>
