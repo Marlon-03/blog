0b74afe319a98997bbdd0aef913902ef400b5953
Marlon-03 <marlonmovilla03@gmail.com>
2024-02-13 09:17:23 +0800

    delete store.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,7 @@ import App from '../js/App.vue';
 import '../css/app.css'
 import '../css/style.css'
 import router from './router/index.js';
-import store from './store'; // make sure the path is correct
 
 createApp(App)
   .use(router)
-  .use(store) // add this line
   .mount('#app');
